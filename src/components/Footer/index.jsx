fa800092b6a069bee02e92c3b0a0756195d1a26c
fdd <kristiyan.ivanov.98@gmail.com>
2026-03-06 01:08:33 +0200

    Changed footer list items to dummy links instead of plain text

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,21 @@ const Footer = () => {
     return (
         <div className="bg-(--accent-color) py-4 text-white font-medium">
             <ul className="w-fit mx-auto">
-                <li className="list-disc list-inside">Terms & Conditions</li>
-                <li className="list-disc list-inside">Privacy Policy</li>
-                <li className="list-disc list-inside">Contact Us</li>
+                <li className="list-disc list-inside">
+                    <a href="_blank">
+                        Terms & Conditions
+                    </a>
+                </li>
+                <li className="list-disc list-inside">
+                    <a href="_blank">
+                        Privacy Policy
+                    </a>
+                </li>
+                <li className="list-disc list-inside">
+                    <a href="_blank">
+                        Contact Us
+                    </a>
+                </li>
             </ul>
         </div>
     );
